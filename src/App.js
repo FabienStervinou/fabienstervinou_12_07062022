63248bba77086b1ab6e1b5cb90b5e8907d71b098
Fabien Stervinou <stervinoufabien@gmail.com>
2022-06-10 09:50:21 +0200

    Create App routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import './App.css';
-import Logo from '../src/assets/img/logo-sportsee.svg'
+import './App.scss';
+import Header from './layout/Header/index.js';
+import AsideMenu from './layout/AsideMenu/index.js';
+import Home from './views/Home/index';
+import Error from './views/Error/index';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={Logo} alt="Logo de Sport See" />
-        <p>
-          SportSee
-        </p>
-      </header>
+      <BrowserRouter>
+        <Header />
+        <AsideMenu />
+          <Routes>
+            <Route exact path="/" element={<Home/>} />
+            <Route exact path="/user/:userId" element={<Home/>} /> 
+            <Route exact path="/404" element={<Error/>} /> 
+            <Route exact path="*" element={<Navigate to="/404" replace={true} />} />
+          </Routes>
+      </BrowserRouter>
     </div>
   );
 }
