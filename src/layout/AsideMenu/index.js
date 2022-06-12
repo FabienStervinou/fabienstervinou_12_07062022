6287f331b8437abcb4526131a47f7a77ebe8cf58
Fabien Stervinou <stervinoufabien@gmail.com>
2022-06-12 17:40:13 +0200

    Create <AsideMenu> component

---
+++ src/layout/AsideMenu/index.js
@@ -0,0 +1,31 @@
+import './index.scss';
+import Bike from '../../assets/img/bike.svg'
+import Panting from '../../assets/img/panting.svg'
+import Swim from '../../assets/img/swim.svg'
+import Yoga from '../../assets/img/yoga.svg'
+
+function AsideMenu() {
+  return ( 
+    <aside className="aside">
+      <div className='asideContainer'>
+        <a href="#" className="asideButton">
+          <img src={Bike} alt="" />
+        </a>
+        <a href="#" className="asideButton">
+          <img src={Panting} alt="" />
+        </a>
+        <a href="#" className="asideButton">
+          <img src={Swim} alt="" />
+        </a>
+        <a href="#" className="asideButton">
+          <img src={Yoga} alt="" />
+        </a>
+      </div>
+      <p className="copyright">
+        Copiryght, SportSee 2020
+      </p>
+    </aside>
+  );
+}
+
+export default AsideMenu;
