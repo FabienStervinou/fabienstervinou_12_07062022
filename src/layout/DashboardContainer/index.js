60cc9bc7ba00cad7c2e9bffb36f90a06a3b32d9b
Fabien Stervinou <stervinoufabien@gmail.com>
2022-06-13 11:00:18 +0200

    Create <DashboardContainer> component

---
+++ src/layout/DashboardContainer/index.js
@@ -0,0 +1,12 @@
+import './styles.scss';
+import Activity from '../../components/Dashboard/Activity/index.js';
+
+function DashboardContainer () {
+  return ( 
+    <div className="dashboardContainer">
+      <Activity/>
+    </div>
+  );
+}
+
+export default DashboardContainer;
