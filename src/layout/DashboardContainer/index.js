a8093fda09fc5330a721f660cd8578813379123e
Fabien Stervinou <stervinoufabien@gmail.com>
2022-06-19 19:45:26 +0200

    Update <DashboardContainer> and css grid style

--- src/layout/DashboardContainer/index.js
+++ src/layout/DashboardContainer/index.js
@@ -1,10 +1,18 @@
 import './styles.scss';
 import Activity from '../../components/Dashboard/Activity/index.js';
+import StatBar from '../../components/Dashboard/statBar/index.js';
+import Objectif from '../../components/Dashboard/Objectif/index.js';
+import Performance from '../../components/Dashboard/Performance/index.js';
+import Score from '../../components/Dashboard/Score/index.js';
 
 function DashboardContainer () {
   return ( 
     <div className="dashboardContainer">
-      <Activity/>
+      <Activity />
+      <StatBar />
+      <Objectif />
+      <Performance />
+      <Score />
     </div>
   );
 }
