ff7a446d4ff2cd9ffd375f12be6a578a08e40f82
Fabien Stervinou <stervinoufabien@gmail.com>
2022-06-21 17:50:46 +0200

    Update <Performance> dashboard component

--- src/components/Dashboard/Performance/index.js
+++ src/components/Dashboard/Performance/index.js
@@ -4,14 +4,25 @@ import { ResponsiveContainer, RadarChart, PolarGrid, PolarAngleAxis, Radar } fro
 
 
 function Performance() {
-  const data = useFetch('performance')
-  console.log('log data :', data)
+  const { data } = useFetch('performance')
+
   return ( 
     <div className="performance">
       <ResponsiveContainer>
-        <RadarChart data={data.data}>
-          <PolarGrid />
-          <PolarAngleAxis dataKey="kind"/>
+        <RadarChart 
+          data={data} 
+          outerRadius={70}
+          >
+          <PolarGrid 
+            radialLines={false}
+          />
+          <PolarAngleAxis 
+            dataKey="kind"
+            tick={{
+              fill: 'white',
+              fontSize: 12
+            }}
+            />
           <Radar dataKey="value" fill="#FF0000" fillOpacity={0.7} />
         </RadarChart>
       </ResponsiveContainer>
